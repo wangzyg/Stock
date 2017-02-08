0ee62e542a049981c9591e7a895f503ada18f174
wangzyg@foxmail.com <wangzy@hxcomm.cn>
2017-02-08 10:23:53 +0800

    初始化

---
+++ src/main/java/com/sam/stock/App.java
@@ -0,0 +1,13 @@
+package com.sam.stock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
